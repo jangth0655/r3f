9c364a568e5c6b3fd0ac2e6345fe2bfe7cefcf26
TAEHEE <jangth0655@gmail.com>
2024-01-06 17:38:50 +0900

    r3f initializing

---
+++ src/components/MainCanvas.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import { Box, OrbitControls } from '@react-three/drei';
+import { Canvas } from '@react-three/fiber';
+import * as THREE from 'three';
+
+export default function MainCanvas() {
+  const aspectRatio = window.innerWidth / window.innerHeight;
+  return (
+    <Canvas
+      id="canvas"
+      gl={{ antialias: true }}
+      shadows="soft"
+      camera={{ fov: 30, aspect: aspectRatio, near: 0.01, far: 1000, position: [0, 6, 12] }}
+      scene={{ background: new THREE.Color(0x000000) }}
+    >
+      <Box material-color={0xff0000} />
+      <OrbitControls />
+    </Canvas>
+  );
+}
